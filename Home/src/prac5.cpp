48b5a9a8bca3c46bd5e2d485582e896434ca44c7
Jay Shimpi <jayshimpi24@gmail.com>
2024-12-11 16:36:57 +0530

    add hilbert curve

---
+++ Home/src/prac5.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<graphics.h>
+using namespace std;
+/*
+Program to generate Hibert Curve using the concept of fractals
+Hilbert Curve:--
+    1. Start with a square of side length 2^n.
+    2. Divide the square into 4 equal parts.
+    3. Draw a curve in the following order:--
+        a. Bottom left part
+        b. Top left part
+        c. Top right part
+        d. Bottom right part
+    4. Repeat the above steps for each part.
+*/
+
+//Function to move the cursor
+void move(int &x, int &y, int dir, int len){
+    switch(dir){
+        case 0: y += len; break;
+        case 1: x += len; break;
+        case 2: y -= len; break;
+        case 3: x -= len; break;
+    }
+    lineto(x, y);   
+}
+
+//Function to draw the Hilbert Curve 
+void hilbert(int r, int d, int l, int u, int i, int h, int &x, int &y) {
+    if (i > 0) {
+        i--; // Decrement recursion level
+        
+        // Draw smaller curves and move as per the Hilbert curve logic
+        hilbert(d, r, u, l, i, h, x, y); // 1st quadrant
+        move(r, h, x, y);                // Move in the "right" direction
+        hilbert(r, d, l, u, i, h, x, y); // 2nd quadrant
+        move(d, h, x, y);                // Move in the "down" direction
+        hilbert(r, d, l, u, i, h, x, y); // 3rd quadrant
+        move(l, h, x, y);                // Move in the "left" direction
+        hilbert(u, l, d, r, i, h, x, y); // 4th quadrant
+    }
+}
+
+
+int main(){
+    int gd = DETECT, gm;
+    int n, x1, y1;
+    int x0 = 50, y0 = 150;   // Starting point of the curve
+    int x = x0, y = y0;      // Initial coordinates
+    int h = 10;              // Step size
+    int r = 2, d = 3, l = 4, u = 1; // Directions (right, down, left, up)
+
+    cout << "\nGive the value of n: ";
+    cin >> n;
+
+    moveto(x, y);             // Move to the starting point
+    hilbert(r, d, l, u, n, h, x, y); // Draw the Hilbert curve
+
+    delay(100); // Pause to view the result
+    closegraph(); // Close the graphics window
+    return 0;
+}
