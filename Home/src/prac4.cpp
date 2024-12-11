e1bb373921cc54c3e0cd9d0a9c49ded0b867ce9a
Jay Shimpi <jayshimpi24@gmail.com>
2024-12-11 14:06:30 +0530

    refactor Rec to Object; transform about pivot

--- Home/src/prac4.cpp
+++ Home/src/prac4.cpp
@@ -1,72 +1,99 @@
-#include<iostream>
-#include<graphics.h>
-#include<math.h>
+#include <iostream>
+#include <graphics.h>
+#include<vector>
+#include <math.h>
 using namespace std;
 /*
-Program to draw 2-D object and perform following basic transformations, 
+Program to draw 2-D object and perform following basic transformations,
 a) Scaling b) Translation c) Rotation. Apply the concept of operator overloading.
 */
 
+class Object
+{
+    vector <int> points;
+    int n;
+    int pivotX, pivotY;
 
-class Rec{
-    int x1, y1, x2, y2;
-    public:
-    Rec(int x1, int y1, int x2, int y2){
-        this->x1 = x1;
-        this->y1 = y1;
-        this->x2 = x2;
-        this->y2 = y2;
+
+public:
+    Object(vector <int> p)
+    {
+        points = p;
+        n = p.size();
+        pivotX = points[0];
+        pivotY = points[1];
     }
 
-    void draw(){
-        rectangle(x1, y1, x2, y2);
+    void draw()
+    {
+        for (int i = 0; i < n; i += 2)
+        {
+            line(points[i], points[i + 1], points[(i + 2) % n], points[(i + 3) % n]);
+        }
     }
 
-    //overload * operator to scale
-    Rec operator*(float s){
-        return Rec(x1 * s, y1 * s, x2 * s, y2 * s);
+    // overload * operator to scale
+    Object operator*(float s)
+    {
+        vector <int> p;
+        for (int i = 0; i < n; i+=2)
+        {
+            p.push_back(pivotX + (points[i] - pivotX) * s);
+            p.push_back(pivotY + (points[i + 1] - pivotY) * s);
+        }
+        return Object(p);
     }
 
-    //overload + operator to translate
-    Rec operator+(int t){
-        return Rec(x1 + t, y1 + t, x2 + t, y2 + t);
+    // overload + operator to translate
+    Object operator+(int t)
+    {
+        vector <int> p;
+        for (int i = 0; i < n; i += 2)
+        {
+            p.push_back(points[i] + t);
+            p.push_back(points[i + 1] + t);
+        }
+        return Object(p);
     }
 
-    //overload % operator to rotate
-    Rec operator%(int angle){
-        float theta = angle * 3.14159 / 180;
-        int x = (x1 + x2) / 2;
-        int y = (y1 + y2) / 2;
-        int x1 = x + (x1 - x) * cos(theta) - (y1 - y) * sin(theta);
-        int y1 = y + (x1 - x) * sin(theta) + (y1 - y) * cos(theta);
-        int x2 = x + (x2 - x) * cos(theta) - (y2 - y) * sin(theta);
-        int y2 = y + (x2 - x) * sin(theta) + (y2 - y) * cos(theta);
-        return Rec(x1, y1, x2, y2);
+    // overload % operator to rotate
+    Object operator%(int angle)
+    {
+        vector <int> p;
+        float rad = angle * 3.14159 / 180;
+        for (int i = 0; i < n; i += 2)
+        {
+            // rotate about pivot point
+            p.push_back(pivotX + (points[i] - pivotX) * cos(rad) - (points[i + 1] - pivotY) * sin(rad));
+            p.push_back(pivotY + (points[i] - pivotX) * sin(rad) + (points[i + 1] - pivotY) * cos(rad));
+        }
+        return Object(p);
     }
 };
 
-int main(){
+int main()
+{
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
 
-    Rec r(100, 100, 200, 250);
+    vector <int> p = {100, 100, 200, 100, 200, 200, 100, 200};
+    Object r(p);
+    r.draw();
+    getch();
+
+    cleardevice();
+    r = r + 50; // translate by 50 units, call overloaded + operator
     r.draw();
     getch();
 
 
     cleardevice();
-    r = r + 50; //translate by 50 units, call overloaded + operator
-    r.draw();
-    getch();
-
-
-    cleardevice();
-    r = r * 2; //scale by 2, call overloaded * operator
+    r = r * 2; // scale by 2, call overloaded * operator
     r.draw();
     getch();
 
     cleardevice();
-    r = r % 60; //rotate by 60 degrees, call overloaded % operator
+    r = r % 30; // rotate by 30 degrees, call overloaded % operator
     r.draw();
     getch();
 
